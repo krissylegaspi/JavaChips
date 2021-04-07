5cfce0c93b4b754a5a6a3b86699b1f3bc012598f
Kristine Legaspi <krissylegaspi@gmail.com>
2021-04-06 22:28:17 -0700

    Planted a tree

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+    public static void main(String[] args) {
+        // Introduce myself and print a tree on a screen
+        System.out.println("Hello, I'm Kristine and I'm learning to code in Java.");
+        System.out.println("I'm going to plant a tree today.");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
