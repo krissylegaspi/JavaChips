1efb0375917a510657ed60dc54facb32fdd836ad
Kristine Legaspi <krissylegaspi@gmail.com>
2021-05-02 19:29:46 -0700

    Add coding exercise 13

---
+++ Java Programming Masterclass/Coding Exercise 13 - NumberOfDaysInMonth/src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+
+    public static void main(String[] args) {
+        isLeapYear(-1600);
+        isLeapYear(1600);
+        isLeapYear(2017);
+        isLeapYear(2000);
+        isLeapYear(9000);
+        getDaysInMonth(1, 2020);
+        getDaysInMonth(2, 2020);
+        getDaysInMonth(2, 2018);
+        getDaysInMonth(-1, 2020);
+        getDaysInMonth(1, -2020);
+    }
+
+    public static boolean isLeapYear (int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0 && year % 100 != 0) {
+                return true;
+            }
+            else if (year % 400 == 0 && year % 100 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        }
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 2:
+                if (isLeapYear(year)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            case 4: case 6: case 9: case 11:
+                return 30;
+            default:
+                return -1;
+        }
+    }
+}
