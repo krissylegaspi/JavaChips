eed22aac060c28f1255fbe9a596141687cbc9e76
Kristine Legaspi <krissylegaspi@gmail.com>
2021-05-02 17:14:49 -0700

    Learned about switch statements

--- Java Programming Masterclass/Switch/src/Main.java
+++ Java Programming Masterclass/Switch/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("Was not 1 or 2");
         }
 
-	    int switchValue = 1;
+	    int switchValue = 3;
 
 	    switch(switchValue) {
             case 1:
@@ -20,6 +20,10 @@ public class Main {
                 break;
             case 2:
                 System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, or a 4, or a 5");
+                System.out.println("Actually, it was a " + switchValue);
                 break;
             default:
                 System.out.println("Was not 1 or 2");
